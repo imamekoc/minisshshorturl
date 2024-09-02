edbbf5b49f507f036e5b8f2a12f470b4ed5ab261
Tino Imam M P <tinoimammp@gmail.com>
2024-09-03 05:50:43 +0700

    footer

--- components/footer.tsx
+++ components/footer.tsx
@@ -3,15 +3,15 @@ const Footer = () => {
     <div className='supports-backdrop-blur:bg-background/60 z-20 w-full shadow bg-background/95 backdrop-blur'>
       <div className='mx-8 flex h-14 items-center'>
         <small className='text-muted-foreground'>
-          Created by{' '}
+          Created by Minicut.link{' '}
           <a
             className='hover:underline'
-            href='https://www.salimi.my/'
-            aria-label='Salimi'
+            href='https://minicut.link/'
+            aria-label='MiniSSH'
             target='_blank'
             rel='noreferrer'
           >
-            Salimi
+            Part of MiniSSH
           </a>{' '}
           &copy; {new Date().getFullYear()}. All right reserved.
         </small>
